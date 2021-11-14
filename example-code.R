c47d9fba6949210d0fe4fa0f7da7e96368161405
Eliran Soffer <eliranso@wismain.weizmann.ac.il>
2021-11-14 20:55:03 +0200

    Code for Ben merge two tables

--- example-code.R
+++ example-code.R
@@ -1,1 +1,6 @@
-library("ggplot")
+library("readxl")
+
+df1 <- read_excel("df1.xlsx") ## This is big table #1
+df2 <- read_excel("df2.xlsx") ## Thsi is big table #2, same column names but different order
+
+df.merged <- rbind(df1,df2) ## Create new data frame of both tables but keeping the order of columns as in df1
